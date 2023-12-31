2f7ecaf4e0dbfb35ef15d3ef521b17b4e92d0fff
JorshBG <jordiyair29@gmail.com>
2023-12-30 19:05:42 -0600

    change icons

--- src/ts/export/projects.ts
+++ src/ts/export/projects.ts
@@ -1,35 +1,73 @@
 const projects: Array<Project> = [
-    {
-        name:"Cima",
-        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam cum iure rerum maiores aspernatur aliquid dolor explicabo soluta distinctio doloribus, ipsum, reprehenderit repellendus excepturi a in perferendis nulla unde exercitationem.",
-        image:{src:"https://randomfox.ca/images/119.jpg", alt:"image"},
-        technologies: [{name:"mdi:docker", tooltip:"Docker"}, {name:'simple-icons:php', tooltip:"PHP v7.2"}, {name:"mdi:bootstrap", tooltip:"Bootstrap 4"}, {name:"ic:outline-javascript", tooltip:"JavaScript Vanilla"}]
-    },
-    {
-        name:"Basic Paint",
-        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam cum iure rerum maiores aspernatur aliquid dolor explicabo soluta distinctio doloribus, ipsum, reprehenderit repellendus excepturi a in perferendis nulla unde exercitationem.",
-        image:{src:"https://randomfox.ca/images/19.jpg", alt:"image"},
-        technologies: [{name:'simple-icons:csharp', tooltip:"C# core"}]
-    },
-    {
-        name:"Kanban Board Client",
-        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam cum iure rerum maiores aspernatur aliquid dolor explicabo soluta distinctio doloribus, ipsum, reprehenderit repellendus excepturi a in perferendis nulla unde exercitationem.",
-        image:{src:"https://randomfox.ca/images/90.jpg", alt:"image"},
-        technologies: [{name:'simple-icons:react', tooltip:"ReactJS v18"}, {name:'simple-icons:vite', tooltip:"ViteJS"}]
-    },
-    {
-        name:"Kanban Board API",
-        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam cum iure rerum maiores aspernatur aliquid dolor explicabo soluta distinctio doloribus, ipsum, reprehenderit repellendus excepturi a in perferendis nulla unde exercitationem.",
-        image:{src:"https://randomfox.ca/images/82.jpg", alt:"image"},
-        technologies: [{name:'simple-icons:php', tooltip:"PHP 8.1"}, {name:"mdi:laravel", tooltip:"Laravel v10"}]
-    },
-    {
-        name:"Mini Forms",
-        description:"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam cum iure rerum maiores aspernatur aliquid dolor explicabo soluta distinctio doloribus, ipsum, reprehenderit repellendus excepturi a in perferendis nulla unde exercitationem.",
-        image:{src:"https://randomfox.ca/images/82.jpg", alt:"image"},
-        technologies: [{name:'simple-icons:php', tooltip:"PHP 8.1"}, {name:"ic:outline-javascript", tooltip:"JavaScript Vanilla"}, {name:"mdi:bootstrap", tooltip:"Bootstrap 5"}]
-    }
+  {
+    name: "Cima",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam cum iure rerum maiores aspernatur aliquid dolor explicabo soluta distinctio doloribus, ipsum, reprehenderit repellendus excepturi a in perferendis nulla unde exercitationem.",
+    image: { src: "https://randomfox.ca/images/119.jpg", alt: "image" },
+    technologies: [
+      { name: "mdi:docker", tooltip: "Docker" },
+      { name: "simple-icons:php", tooltip: "PHP v7.2" },
+      { name: "mdi:bootstrap", tooltip: "Bootstrap 4" },
+      { name: "ic:outline-javascript", tooltip: "JavaScript Vanilla" },
+      { name: "simple-icons:mysql", tooltip: "MySQL 5.7" },
+    ],
+  },
+  {
+    name: "Access Control",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam cum iure rerum maiores aspernatur aliquid dolor explicabo soluta distinctio doloribus, ipsum, reprehenderit repellendus excepturi a in perferendis nulla unde exercitationem.",
+    image: { src: "https://randomfox.ca/images/82.jpg", alt: "image" },
+    technologies: [
+      { name: "simple-icons:php", tooltip: "PHP 8.2" },
+      { name: "simple-icons:cplusplus", tooltip: "C++" },
+      { name: "simple-icons:arduino", tooltip: "Arduino" },
+      { name: "mdi:tailwind", tooltip: "Tailwind v3.4" },
+      { name: "simple-icons:sqlite", tooltip: "SQLite" },
+    ],
+  },
+  {
+    name: "Basic Paint",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam cum iure rerum maiores aspernatur aliquid dolor explicabo soluta distinctio doloribus, ipsum, reprehenderit repellendus excepturi a in perferendis nulla unde exercitationem.",
+    image: { src: "https://randomfox.ca/images/19.jpg", alt: "image" },
+    technologies: [
+      { name: "mdi:language-csharp", tooltip: "C#" },
+      { name: "simple-icons:dotnet", tooltip: "dotnet" },
+    ],
+  },
+  {
+    name: "Kanban Board Client",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam cum iure rerum maiores aspernatur aliquid dolor explicabo soluta distinctio doloribus, ipsum, reprehenderit repellendus excepturi a in perferendis nulla unde exercitationem.",
+    image: { src: "https://randomfox.ca/images/90.jpg", alt: "image" },
+    technologies: [
+      { name: "simple-icons:react", tooltip: "ReactJS v18" },
+      { name: "simple-icons:vite", tooltip: "ViteJS" },
+    ],
+  },
+  {
+    name: "Kanban Board API",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam cum iure rerum maiores aspernatur aliquid dolor explicabo soluta distinctio doloribus, ipsum, reprehenderit repellendus excepturi a in perferendis nulla unde exercitationem.",
+    image: { src: "https://randomfox.ca/images/82.jpg", alt: "image" },
+    technologies: [
+      { name: "simple-icons:php", tooltip: "PHP 8.1" },
+      { name: "mdi:laravel", tooltip: "Laravel v10" },
+      { name: "simple-icons:mysql", tooltip: "MySQL 8" },
+    ],
+  },
+  {
+    name: "Mini Forms",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam cum iure rerum maiores aspernatur aliquid dolor explicabo soluta distinctio doloribus, ipsum, reprehenderit repellendus excepturi a in perferendis nulla unde exercitationem.",
+    image: { src: "https://randomfox.ca/images/82.jpg", alt: "image" },
+    technologies: [
+      { name: "simple-icons:php", tooltip: "PHP 8.1" },
+      { name: "ic:outline-javascript", tooltip: "JavaScript Vanilla" },
+      { name: "mdi:bootstrap", tooltip: "Bootstrap 5" },
+      { name: "simple-icons:mysql", tooltip: "MySQL 8" },
+    ],
+  },
 ];
-
 
 export default projects;
