3b2364ca47d5cda21a89e43189ba38942993f81e
JorshBG <jordiyair29@gmail.com>
2023-12-30 19:11:41 -0600

    Init project

--- src/env.d.ts
+++ src/env.d.ts
@@ -15,14 +15,29 @@ interface TechIcon {
     hover?:string,
 }
 
+interface Tech {
+    name:string;
+    tooltip:string
+}
+
 interface Project {
     name: string;
     description:string;
-    technologies:Array<{name:string, tooltip:string}>;
+    technologies:Array<Tech>;
     image:{src:string;alt:string}
 }
 
 interface ContentTabs {
     name:string;
     id:string;
+}
+
+interface ProExperience {
+    enterprise_name:string;
+    job_title:string;
+    date_start:string;
+    date_end:string;
+    job_description:string;
+    technologies:Array<Tech>;
+    goals:Array<string>;
 }
---
+++ src/ts/export/professionalExperience.ts
@@ -0,0 +1,13 @@
+const professionalExperience: Array<ProExperience> = [
+{
+    job_title:"",
+    job_description:"",
+    enterprise_name:"",
+    date_end:"",
+    date_start:"",
+    technologies:[],
+    goals:[]
+}
+];
+
+export default professionalExperience;
